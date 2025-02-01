ece2eb290e9fad0ba5d88dd1147ac3d57db3979c
Visal Nagahawatte <132425926+visalDiasNagahawatte@users.noreply.github.com>
2025-02-01 14:37:58 +0530

    Update: intialize project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://root:root123@book-store.fopof.mongodb.net/book-collection?retryWrites=true&w=majority&appName=book-store";
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(225).send("Good");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error");
+  });
